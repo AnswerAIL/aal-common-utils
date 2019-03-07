3090af4b16bf3bb83b15a5ac1a0b117cc25dfd12
L.Answer <answer_ljm@163.com>
2019-03-07 10:16:39 +0800

    first commit

---
+++ src/main/java/org/answer/common/util/exception/AIException.java
@@ -0,0 +1,16 @@
+package org.answer.common.util.exception;
+
+/**
+ * Created by Answer.AI.L on 2019-03-07 09:46
+ */
+public class AIException extends RuntimeException {
+
+    public AIException() {
+        super();
+    }
+
+    public AIException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/org/answer/common/util/iso8583/ISO8583Annotation.java
@@ -0,0 +1,53 @@
+package org.answer.common.util.iso8583;
+
+import java.lang.annotation.*;
+
+/**
+ * ISO8583字段域注解类
+ * Created by Answer on 2017-12-21 09:45
+ */
+@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ISO8583Annotation {
+
+    /**
+     * 域索引  MUST
+     * */
+    int fldIndex();
+
+    /**
+     * 数据域长度    MUST
+     * */
+    int dataFldLength();
+
+    /**
+     * 域编码规则(ASCII/BCD/HEX/BINARY)   MUST
+     * */
+    String encodeRule();
+
+    /**
+     * 域字段标识(0: 不用 1: 长度固定 2: 2位变长 3: 3位变长)  MUST
+     * */
+    String fldFlag();
+
+    /**
+     * 域长度编码规则(ASCII/BCD[默认]/HEX/BINARY)
+     * */
+    String lenEncodeRule() default "BCD";
+
+    /**
+     * 域填充规则(NONE(默认)/AFTER/BEFORE)
+     * */
+    String fillRule() default "NONE";
+
+    /**
+     * 域填充字符,十六进制ASSIC码(0:30, 空格:20)
+     * */
+    String fillChar() default "";
+
+    /**
+     * 域默认值
+     * */
+    String defalutValue() default "";
+}
