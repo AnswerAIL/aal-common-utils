80238395b5b9e2df0b65175a0b2955d613efdf01
L.Answer <answer_ljm@163.com>
2019-05-14 19:15:04 +0800

    alibaba easyexcel usage

--- src/main/java/org/answer/common/util/easyexcel/AnalysisParser.java
+++ src/main/java/org/answer/common/util/easyexcel/AnalysisParser.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class AnalysisParser<T extends AnalysisParser> extends AnalysisEventListener {
 
-    // excel 数据集
+    /** excel 数据集 */
     protected List<List<String>> datas;
 
     public AnalysisParser() {
