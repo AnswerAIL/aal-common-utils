d1407f02c57d82601fec59942a65654864ae3521
L.Answer <answer_ljm@163.com>
2019-05-22 20:06:54 +0800

    自定义解析器

--- src/test/java/org/answer/common/util/excel/EasyExcelApp.java
+++ src/test/java/org/answer/common/util/excel/EasyExcelApp.java
@@ -1,6 +1,7 @@
 package org.answer.common.util.excel;
 
 import com.alibaba.fastjson.JSON;
+import org.answer.common.util.easyexcel.read.AnalysisParser;
 import org.answer.common.util.easyexcel.read.ExcelUtils;
 
 import java.io.File;
@@ -16,6 +17,17 @@ public class EasyExcelApp {
 
     public static void main(String[] args) throws FileNotFoundException {
         List<User> datas = ExcelUtils.readXlsExcel(new File("src/test/resources/users.xls"), new UserAnalysisParser(User.class));
+
+        for (User data: datas) {
+            System.out.println(JSON.toJSONString(data));
+        }
+
+        System.out.println();
+        System.out.println("##################################################");
+        System.out.println();
+
+
+        datas = ExcelUtils.readXlsExcel(new File("src/test/resources/users.xls"), new AnalysisParser<>(User.class));
 
         for (User data: datas) {
             System.out.println(JSON.toJSONString(data));
