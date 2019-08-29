7437b1b44ab29f5ed547b717f84ff65fc05f11dc
L.Answer <answer_ljm@163.com>
2019-08-29 13:41:05 +0800

    日期转换指定格式

--- src/main/java/org/answer/common/util/date/Date2FmtUtils.java
+++ src/main/java/org/answer/common/util/date/Date2FmtUtils.java
@@ -23,6 +23,17 @@ import java.util.Map;
 //@Slf4j
 public class Date2FmtUtils {
 
+    private static final Map<String, String> VARIS = Maps.newHashMap();
+    static {
+        VARIS.put("yyyy", "2019");
+        VARIS.put("MM", "01");
+        VARIS.put("dd", "01");
+        VARIS.put("HH", "00");
+        VARIS.put("mm", "00");
+        VARIS.put("ss", "00");
+        VARIS.put("SSS", "001");
+    }
+
     public static void main(String[] args) {
 
 //        String format = "yyyy-MM-dd HH:mm:ss";
@@ -32,7 +43,8 @@ public class Date2FmtUtils {
         System.out.println("4=>" + toDateStr("2019-08-01", "yyyy"));
         System.out.println("5=>" + toDateStr("2019-08-01 01", "yyyy-MM-dd HH:mm:ss"));
         System.out.println("6=>" + toDateStr("2019080101", "yyyy-MM-dd HH:mm:ss"));
-        System.out.println("7=>" + toDateStr("201908", "yyyy-MM-dd HH:mm:ss"));
+        System.out.println("7=>" + toDateStr("201908", "yyyy-MM-dd HH:mm:ss.SSS"));
+        System.out.println("8=>" + toDateStr("2019080101", "yyyy-MM-dd HH:mm:ss.SSS"));
     }
 
 
@@ -45,26 +57,22 @@ public class Date2FmtUtils {
      */
     public static String toDateStr(String date, String format) {
 //        log.info("转换前 date=[{}], format=[{}]", date, format);
-        if (date.length() > 19 || format.length() > 19) {
-//            log.warn("日期及日期格式长度最大不能超过19, date=[{}], format=[{}]", date, format);
+        if (date.length() > 23 || format.length() > 23) {
+//            log.warn("日期及日期格式长度最大不能超过23, date=[{}], format=[{}]", date, format);
             return null;
         }
         String rlt;
 
-        Map<String, String> varis = Maps.newHashMap();
-        varis.put("yyyy", "2019");varis.put("MM", "01");varis.put("dd", "01");
-        varis.put("HH", "00");varis.put("mm", "00");varis.put("ss", "00");
-
         if (date.length() != format.length()) {
-            String ft = format.replaceAll("[- :]", "");
+            String ft = format.replaceAll("[- :.]", "");
             if (format.length() < date.length()) {
                 date = date.substring(0, format.length());
             }
-            date = date.replaceAll("[- :]", "");
+            date = date.replaceAll("[- :.]", "");
 
             date = date + ft.substring(date.length(), ft.length());
-            for (String key: varis.keySet()) {
-                String value = varis.get(key);
+            for (String key: VARIS.keySet()) {
+                String value = VARIS.get(key);
                 date = date.replaceAll(key, value);
             }
             rlt = date2Fmt(date, format);
@@ -88,8 +96,10 @@ public class Date2FmtUtils {
         String[] as = format.split("");
         for (int i = 0, j = 0; i < as.length; i++) {
             String e = as[i];
-            if ("-".equals(e) || ":".equals(e) || " ".equals(e)) {
+            if ("-".equals(e) || ":".equals(e) || ".".equals(e)) {
                 sb.append(e);
+            } else if (" ".equals(e)) {
+                sb.append("T");
             } else {
                 sb.append(date.substring(j, j + 1));
                 j++;
