b40a2c144fb049f1acfaab1dca8f43403c5956c4
L.Answer <answer_ljm@163.com>
2019-08-30 11:10:56 +0800

    日期转换指定格式

--- src/main/java/org/answer/common/util/date/Date2FmtUtils.java
+++ src/main/java/org/answer/common/util/date/Date2FmtUtils.java
@@ -9,6 +9,12 @@ package org.answer.common.util.date;
 
 import com.google.common.collect.Maps;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -45,6 +51,9 @@ public class Date2FmtUtils {
         System.out.println("6=>" + toDateStr("2019080101", "yyyy-MM-dd HH:mm:ss"));
         System.out.println("7=>" + toDateStr("201908", "yyyy-MM-dd HH:mm:ss.SSS"));
         System.out.println("8=>" + toDateStr("2019080101", "yyyy-MM-dd HH:mm:ss.SSS"));
+        System.out.println("9=>" + toDateStr("2019-08-29T00:00:00+08:00", "yyyy-MM-dd HH:mm:ss.SSS"));
+        System.out.println("10=>" + toDateStr("2019年08月29日T00时00:00", "yyyy-MM-dd HH:mm:ss"));
+        System.out.println("11=>" + toDateStr("Thu Aug 29 15:54:50 CST 2019", "yyyy-MM-dd HH:mm:ss"));
     }
 
 
@@ -56,10 +65,18 @@ public class Date2FmtUtils {
      * @return date
      */
     public static String toDateStr(String date, String format) {
+        date = date.endsWith("+08:00") ? date.replaceAll("\\+08\\:00", "") : date;
+        date = date.replaceAll("[年月]", "-").replaceAll("[时分]", ":").replaceAll("[日号秒]", "");
 //        log.info("转换前 date=[{}], format=[{}]", date, format);
         if (date.length() > 23 || format.length() > 23) {
-//            log.warn("日期及日期格式长度最大不能超过23, date=[{}], format=[{}]", date, format);
-            return null;
+            try {
+                LocalDateTime localDateTime = LocalDateTime.ofInstant(new Date(date).toInstant(), ZoneId.systemDefault());
+                return localDateTime.format(DateTimeFormatter.ofPattern(format));
+            } catch (Exception e) {
+                e.printStackTrace();
+//                log.warn("日期及日期格式长度最大不能超过23, date=[{}], format=[{}]", date, format);
+                return null;
+            }
         }
         String rlt;
 
@@ -68,7 +85,7 @@ public class Date2FmtUtils {
             if (format.length() < date.length()) {
                 date = date.substring(0, format.length());
             }
-            date = date.replaceAll("[- :.]", "");
+            date = date.replaceAll("[- :.T]", "");
 
             date = date + ft.substring(date.length(), ft.length());
             for (String key: VARIS.keySet()) {
@@ -109,4 +126,59 @@ public class Date2FmtUtils {
     }
 
 
+
+    /**
+     * 日期是否在 year 年内
+     *
+     * @param date 日期
+     * @param format 日期格式
+     * @param years 年数
+     * @return boolean
+     */
+    private static boolean isDate(String date, String format, long years) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDate startDate = LocalDate.now().minusYears(years).withMonth(1).withDayOfMonth(1);
+
+        String fmtDate;
+        try {
+            fmtDate = LocalDate.parse(date).format(formatter);
+        } catch (DateTimeParseException e) {
+//            log.warn("日期=[{}]转格式=[{}]异常, 准备进行适配转换, ex msg=[{}]", date, format, e.getMessage());
+            fmtDate = toDateStr(date, format);
+        }
+
+        if (fmtDate != null) {
+            LocalDate parse = LocalDate.parse(fmtDate, formatter);
+            return parse.isAfter(startDate);
+        }
+        return false;
+    }
+
+
+    /**
+     * 日期时间是否在 year 年内
+     *
+     * @param date 日期
+     * @param format 日期格式
+     * @param years 年数
+     * @return boolean
+     */
+    private static boolean isDateTime(String date, String format, long years) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDateTime startDate = LocalDateTime.now().minusYears(years).withMonth(1).withDayOfMonth(1);
+
+        String fmtDate;
+        try {
+            fmtDate = LocalDateTime.parse(date).format(formatter);
+        } catch (DateTimeParseException e) {
+//            log.warn("日期=[{}]转格式=[{}]异常, 准备进行适配转换, ex msg=[{}]", date, format, e.getMessage());
+            fmtDate = toDateStr(date, format);
+        }
+
+        if (fmtDate != null) {
+            LocalDateTime parse = LocalDateTime.parse(fmtDate, formatter);
+            return parse.isAfter(startDate);
+        }
+        return false;
+    }
 }
