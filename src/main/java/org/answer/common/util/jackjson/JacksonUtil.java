4fd29165fc6911c1ceb7941ff2fe8bde1efd7007
L.Answer <answer_ljm@163.com>
2019-12-23 01:46:27 -0800

    Jaemon Jackson

--- src/main/java/org/answer/common/util/jackjson/JacksonUtil.java
+++ src/main/java/org/answer/common/util/jackjson/JacksonUtil.java
@@ -7,6 +7,7 @@
  */
 package org.answer.common.util.jackjson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -44,7 +45,7 @@ public class JacksonUtil {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         OBJECT_MAPPER.setDateFormat(dateFormat);
 
-
+        /* 建议使用 OBJECT_MAPPER.enable | OBJECT_MAPPER.disable 来设置配置 */
         // 美化输出
         OBJECT_MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
 
@@ -75,6 +76,9 @@ public class JacksonUtil {
 
         // 将内容包裹为一个 JSON 属性, 属性名由 @JsonRootName 注解指定
         OBJECT_MAPPER.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
+
+        // 不返回 null 值字段
+        OBJECT_MAPPER.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
     }
 
 
@@ -129,6 +133,8 @@ public class JacksonUtil {
 
 
     /**
+     * json 字符串转 对象(包含内部类)
+     *
      * @param json json 字符串
      * @param clazz 需要转换的类
      * @param innerParameterClass clazz 内部泛型类
@@ -141,6 +147,8 @@ public class JacksonUtil {
     }
 
     /**
+     * json数组字符串 转 List
+     *
      * @param jsonArray json 数组字符串
      * @return 对象T的集合
      * @throws IOException .
@@ -150,6 +158,8 @@ public class JacksonUtil {
     }
 
     /**
+     * map 字符串转 map 对象
+     *
      * @param jsonMap json 哈希字符串
      * @return K-V 哈希表
      * @throws IOException .
@@ -159,11 +169,21 @@ public class JacksonUtil {
     }
 
 
+    /**
+     * 对象 转 对象
+     *
+     * @param fromValue 对象参数
+     * @param toValueType 转换类型
+     * @return rlt
+     */
+    public static <T> T convertValue(Object fromValue, Class<T> toValueType) {
+        return OBJECT_MAPPER.convertValue(fromValue, toValueType);
+    }
 
 
 
     /**
-     * 写为字符串
+     * 对象 转 json 字符串
      *
      * @param value 值
      * @return rlt
@@ -173,7 +193,7 @@ public class JacksonUtil {
     }
 
     /**
-     * 写为文件
+     * 对象 转 文件
      *
      * @param file 文件
      * @param value 值
@@ -184,7 +204,7 @@ public class JacksonUtil {
     }
 
     /**
-     * 写为字节流
+     * 对象 转 字节数组
      *
      * @param value 值
      * @return .
